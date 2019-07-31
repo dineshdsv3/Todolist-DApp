42df6720c1de94119c5467b9d22516e7cabc27b2
dineshdsv3 <dineshdsv3@gmail.com>
2019-08-01 01:07:26 +0530

    Smart Contract Updated with create a task functionaality

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,24 @@ pragma solidity 0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+// declaring an arbitrary value of task using struct
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+// Storing the content in the blockchain along with the id
+    mapping(uint =>Task) public tasks;
+// Making the first todlist
+    constructor () public {
+        createTask("This is your first todoList");
+    }
+
+// Function to create a task
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
+
+
 }
